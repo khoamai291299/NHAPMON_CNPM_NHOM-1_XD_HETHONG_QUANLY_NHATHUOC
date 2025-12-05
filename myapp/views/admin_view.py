98b57ccef1d1d646491f0ff1919bd47221b24685
khoamai291299 <khoamai2912@gmail.com>
2025-12-05 21:21:44 +0700

    Template Admin: Update and Add file dashboard, 404, 500

--- myapp/views/admin_view.py
+++ myapp/views/admin_view.py
@@ -4,9 +4,64 @@ from django.db import connection
 
 def index(request):
     if 'user_id' not in request.session:
-        return redirect('adminpanel: admin_login')
+        return redirect('adminpanel:admin_login')
 
     return render(request, 'admin/index.html')
+
+def admin_bill(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/bill.html')
+
+def admin_category(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/category.html')
+
+def admin_customer(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/customer.html')
+
+def admin_employee(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/employee.html')
+
+def admin_permissions(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/permissions.html')
+
+def admin_product(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/product.html')
+
+def admin_roles(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/roles.html')
+
+def admin_users(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/users.html')
+
+def admin_dashboard(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/dashboard.html')
+
+def admin_404(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/404.html')
+
+def admin_500(request):
+    if 'user_id' not in request.session:
+        return redirect('adminpanel:admin_login')
+    return render(request, 'admin/500.html')
 
 def admin_login(request):
     if request.method == 'POST':
