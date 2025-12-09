c2a8680bf4cf3ae5f413410f0dcfc3648c2d50b7
hiennguyen2501 <hienng250104@gmail.com>
2025-12-09 22:08:31 +0700

    add warehouse

---
+++ myapp/models/warehouse.py
@@ -0,0 +1,8 @@
+from django.db import models
+from .employee import Employee
+
+class WarehouseReceipt(models.Model):
+    id = models.CharField(primary_key=True, max_length=10)
+    inputDay = models.DateField(null=True, blank=True)
+    eid = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    totalImport = models.IntegerField(default=0)
