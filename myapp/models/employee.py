88dc5df7210dd6adec275cea10cd4f54596cd869
hangnguyen-4595 <nthang59205@gmail.com>
2025-12-09 21:49:46 +0700

    add employee

---
+++ myapp/models/employee.py
@@ -0,0 +1,19 @@
+from django.db import models
+from .department import Department
+from .position import Position
+
+class Employee(models.Model):
+    id = models.CharField(primary_key=True, max_length=10)
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    address = models.CharField(max_length=50, null=True, blank=True)
+    startday = models.DateField(null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    sex = models.BooleanField(null=True)
+    salary = models.IntegerField(null=True)
+
+    did = models.ForeignKey(Department, on_delete=models.CASCADE)
+    pid = models.ForeignKey(Position, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
